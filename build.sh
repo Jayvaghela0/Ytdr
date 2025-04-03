504fc8ae075a802e181e6937fa79e846bc34172b
Jayvaghela0 <120317638+Jayvaghela0@users.noreply.github.com>
2025-04-03 15:22:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Create weights directory
+# Create necessary directories
 mkdir -p weights
+mkdir -p temp_images
 
 # Download model weights
 wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classicalSR_DF2K_s64w8_SwinIR-M_x2.pth -O weights/swinir_lightweight_x2.pth
