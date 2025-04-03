c79fd485150b7671638fb7ae44f56152fb017cf7
Jayvaghela0 <120317638+Jayvaghela0@users.noreply.github.com>
2025-04-03 15:11:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# Create weights directory
+mkdir -p weights
+
+# Download model weights
+wget https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/001_classicalSR_DF2K_s64w8_SwinIR-M_x2.pth -O weights/swinir_lightweight_x2.pth
+
+# Install dependencies
 pip install -r requirements.txt
